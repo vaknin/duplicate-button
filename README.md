564bcea6a19fc2293f015b2b1341bea52384791b
vaknin <vaknins33@gmail.com>
2019-11-19 21:18:29 +0200

    some minor changes and readme.md

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
-# Log.Works - an open-source chrome extension to search inside Travolutionary logs  
+# Adds a duplicate button to the backoffice
 
-### The app features three available themes:  
-[themes]: https://github.com/vaknin/Log.Works/blob/master/showcase/Themes.gif?raw=true "Theme Selection"
-![Theme Selection][themes]
 
-### Upon opening a sessions page, automatically fetch all files:  
-[loading]: https://github.com/vaknin/Log.Works/blob/master/showcase/Loading.gif?raw=true "Loading"
-![Loading][loading]
-
-### Search inside every logs using a keyword, and optional case-sensitivity:  
-[demo]: https://github.com/vaknin/Log.Works/blob/master/showcase/Demo.gif?raw=true "Demo"
+[demo]: https://i.imgur.com/uNi2sFK.gif "Demo"
 ![Demo][demo]
--- scripts/content.js
+++ scripts/content.js
@@ -4,30 +4,25 @@ async function duplicate(OriginContract){
     // Change value of element, and its children
     function copyValues(origin, target){
 
-        return new Promise(async resolve => {
+        // Copy the value
+        target.value = origin.value;
 
-            // Copy the value
-            target.value = origin.value;
+        // Checkboxes
+        if (origin.checked){
+            target.checked = true;
+        }
+        else{
+            target.checked = false;
+        }
 
-            // Checkboxes
-            if (origin.checked){
-                target.click();
-                target.checked = true;
+        // If the element has children - call this function on them as well
+        if (target.children){
+            for (let child of Array.from(target.children)){
+
+                let index = Array.from(target.children).indexOf(child);
+                copyValues(Array.from(origin.children)[index], child);
             }
-            else{
-                target.checked = false;
-            }
-    
-            // If the element has children - call this function on them as well
-            if (target.children){
-                for (let child of Array.from(target.children)){
-    
-                    let index = Array.from(target.children).indexOf(child);
-                    await copyValues(Array.from(origin.children)[index], child);
-                }
-            }
-            resolve();
-        });
+        }
     }
 
     // Click on "create new contract"
@@ -44,9 +39,9 @@ async function duplicate(OriginContract){
     let NewContract = document.querySelector('.account-box.account-details');
 
     // Copy all values from the original contract
-    await copyValues(OriginContract, NewContract);
+    copyValues(OriginContract, NewContract);
 
-    // Change contract name
+    // Change contract name to the original one + 'clone'
     document.querySelector('[name="Account.AccountAlias"').value = OriginContract.querySelector('[name*="Account.AccountAlias"').value + ' Clone';
 }
 
@@ -54,22 +49,14 @@ async function duplicate(OriginContract){
 function addButtons(contract){
 
     // Create the button
-    let button = contract.querySelector('input[type="button"').cloneNode();
+    let button = document.createElement('input');
+    button.type = 'button';
+    button.classList.add('k-button', 'blue', 'full-width');
     button.style.marginTop = '10px';
     button.value = 'Duplicate';
-    button.addEventListener('click', async () => {
-        
-        // Await for the popup to show
-        while(document.querySelectorAll('.modal-dialog').length != 3){
-            await sleep(10);         
-        }
-        
-        // Click Cancel
-        document.querySelectorAll('.modal-dialog')[0].querySelectorAll('input')[1].click();
 
-        // Copy all of the values from the original contract to the new one
-        duplicate(contract.cloneNode(true));
-    });
+    // Button onclick -> duplicate
+    button.addEventListener('click', () => { duplicate(contract.cloneNode(true)) });
 
     // Add the button
     contract.querySelector('input[type="button"').parentElement.appendChild(button);
@@ -84,11 +71,16 @@ async function sleep(ms) {
 
 // On the backoffice website
 if (document.URL.includes('worldtravelink.com')){
-
     setInterval(() => {
 
         // Check for opened contracts
         if (document.querySelectorAll('.account-box.account-details').length > 0){
+
+            // No need to add the button in the contract creation screen
+            let feedID = document.querySelectorAll('.col-sm-3')[0].innerText.replace(/[^0-9]/g, '');
+            if (feedID == '0'){
+                return;
+            }
 
             let contracts = document.querySelectorAll('.account-box.account-details');
 
